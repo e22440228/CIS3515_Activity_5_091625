19915e669b8aa9bf013bdf5e1a07bea1cb3b6d1b
Chung Ying Lee <e12123484@gmail.com>
2025-09-16 12:24:09 -0400

    complete activity, but textSize has problem

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,46 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import org.w3c.dom.Text
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val c: Context, private val numberArray: Array<Int>) : BaseAdapter() {
+
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numberArray.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numberArray[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+
+        return if(convertView != null){
+            convertView as TextView
+        }else{
+
+            TextView(c)
+
+        }.apply {
+            text = numberArray[position].toString()
+            textSize = 22f
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+
+        return getView(position,convertView,parent).apply{
+            textSize = numberArray[position].toFloat()
+        }
     }
 
 }
